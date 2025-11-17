e3d4dff4c5e53a6679f50d2294915bc1d78f61c8
SyedaNazneen <syedanazneen144@gmail.com>
2025-11-17 17:01:45 +0530

    Final fix: Added root URL redirect to /videos/ page to solve 404

--- MastiAddaProject/urls.py
+++ MastiAddaProject/urls.py
@@ -1,29 +1,3 @@
-"""
-URL configuration for MastiAddaProject project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-# MastiAddaProject/urls.py
-
-from django.contrib import admin
-from django.urls import path, include  # <-- 'include' yahan import hona chahiye
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('videos/', include('coding_zone_app.urls')), # <-- Yeh line zaruri hai
-]
-
 # MastiAddaProject/urls.py
 
 from django.contrib import admin
@@ -35,6 +9,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('videos/', include('coding_zone_app.urls')),
     
-    # Nayi line: Jab koi base URL '/' par aayega, toh usse '/videos/' par redirect kar do.
+    # Yeh line ab base URL (/) ko /videos/ par bhej degi
     path('', RedirectView.as_view(url='videos/', permanent=True), name='index'),
 ]
